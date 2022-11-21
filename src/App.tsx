ee565b6c2b2830a8f26871be3c556bf5869000df
Encall <apichataimimpat@gmail.com>
2022-11-21 21:28:56 +0700

    include chakra-ui

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,20 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import Bicycle_data from "./components/bicycle_card";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const App = () => {
   return(
     <div>
+      <ChakraProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Map />} />
+          <Route path="/bicycle" element={<Bicycle_data />} />
         </Routes>
       </BrowserRouter>
+      </ChakraProvider>
     </div>
   );
 }
