8e247ff4b3abd23ebf61cf1d4e2fac527ca3373e
encall <apichataimimpat@gmail.com>
2022-12-15 04:30:01 +0700

    integrate 0auth

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,17 @@ import Bicycle_data from "./components/bicycle_card";
 import Home from "./pages/home";
 import { ChakraProvider } from "@chakra-ui/react";
 import NavBar from "./components/navbar";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const App = () => {
   return (
     <div>
       <ChakraProvider>
+        <Auth0Provider
+            domain="dev-z73kzw5mg40rj4tu.us.auth0.com"
+            clientId="rpXDYiUrxVIJVXtzTLTRL9oi0Ggkj7QZ"
+            redirectUri={window.location.origin}
+        >
         <BrowserRouter>
           <NavBar />
           <Routes>
@@ -17,6 +23,7 @@ const App = () => {
             <Route path="/bicycle" element={<Bicycle_data />} />
           </Routes>
         </BrowserRouter>
+        </Auth0Provider>
       </ChakraProvider>
     </div>
   );
