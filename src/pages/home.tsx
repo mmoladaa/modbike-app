52eb9e19308a10d573af3b8e1507a36a4ef32232
encall <apichataimimpat@gmail.com>
2022-12-14 23:17:03 +0700

    edit home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,5 @@
 import Map from "../components/Map";
 import Bicycle_card from "../components/bicycle_card";
-import { Flex } from "@chakra-ui/react";
 
 const Home = () => {
   return (
@@ -9,11 +8,12 @@ const Home = () => {
         <div className="z-0">
           <Map />
         </div>
-          <div className="w-full h-0 fixed left-0 bottom-0
+        <div
+          className="w-full h-0 fixed left-0 bottom-0
             flex justify-center items-center
-            " >
-            <Bicycle_card />
-          
+          "
+        >
+          <Bicycle_card />
         </div>
       </div>
     </div>
