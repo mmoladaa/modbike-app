c5a03acd432a66b877946c6205be8d7d47ff150b
mmoladaa <tangmo.n@icloud.com>
2022-12-15 03:11:38 +0700

    edit app for navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import Map from "./components/Map"
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import Map from "./components/Map";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Bicycle_data from "./components/bicycle_card";
 import Home from "./pages/home";
 import { ChakraProvider } from "@chakra-ui/react";
+import NavBar from "./components/navbar";
+
 const App = () => {
-  return(
+  return (
     <div>
       <ChakraProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/map" element={<Map />} />
-          <Route path="/bicycle" element={<Bicycle_data />} />
-        </Routes>
-      </BrowserRouter>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/map" element={<Map />} />
+            <Route path="/bicycle" element={<Bicycle_data />} />
+          </Routes>
+        </BrowserRouter>
       </ChakraProvider>
     </div>
   );
-}
+};
 
 export default App;
