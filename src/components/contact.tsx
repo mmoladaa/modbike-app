58b2428c2765efc843c02aba66f33fe09f323015
mmoladaa <tangmo.n@icloud.com>
2022-12-19 03:42:18 +0700

    add:shadow in card

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -21,102 +21,96 @@ const Contact = () => {
       textAlign="center"
       minH="100vh"
     >
-        <Stack
-          textAlign={"center"}
-          align={"center"}
-          spacing={{ base: 8, md: 10 }}
-          py={{ base: 20, md: 28 }}
+      <Stack
+        textAlign={"center"}
+        align={"center"}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 20, md: 28 }}
+      >
+        <Heading
+          fontWeight={600}
+          fontSize={{ base: "3xl", sm: "4xl", md: "6xl" }}
+          lineHeight={"110%"}
+          bgClip="text"
+          bgGradient="linear(to-r, #FF4E50, #F9D423)"
         >
-          <Heading
-            fontWeight={600}
-            fontSize={{ base: "3xl", sm: "4xl", md: "6xl" }}
-            lineHeight={"110%"}
-            bgClip="text"
-            bgGradient="linear(to-r, #FF4E50, #F9D423)"
-          >
-            Contact
-          </Heading>
-          <Flex
-            overflowY="scroll"
-            gap="10"
-            flexDirection={{ lg: "row", base: "column" }}
-          >
-            <Card boxShadow="xl">
-              <CardHeader>
-                <img src="Puen.png" alt="Puen"></img>
-                <Heading size="md">Puen</Heading>
-              </CardHeader>
-              <CardBody>
-                <Text>
-                  Kittipong Tapyou
-                  <br />
-                  ID | 65070501003
-                </Text>
-              </CardBody>
-              <CardFooter></CardFooter>
-            </Card>
-            <Card>
-              <CardHeader>
-                <img src="Mo.png" alt="Mo"></img>
-                <Heading size="md">Mo</Heading>
-              </CardHeader>
-              <CardBody>
-                <Text>
-                  Natlada Simasathien
-                  <br />
-                  ID | 65070501019
-                </Text>
-              </CardBody>
-              <CardFooter>
-                <Box boxShadow="base" alignContent="center" alignSelf="center" justifyContent="center">
-                  <Button as="a" href="https://www.instagram.com/encall_apichat/" leftIcon={<AiOutlineInstagram />} >Instagram</Button>
-                </Box>
-              </CardFooter>
-            </Card>
-            <Card>
-              <CardHeader>
-                <img src="Tor.png" alt="Tor"></img>
-                <Heading size="md">Tor</Heading>
-              </CardHeader>
-              <CardBody>
-                <Text>
-                  Wongsatorn Sungsilpawech
-                  <br />
-                  ID | 65070501050
-                </Text>
-              </CardBody>
-              <CardFooter></CardFooter>
-            </Card>
-            <Card>
-              <CardHeader>
-                <img src="Yim.png" alt="Yim"></img>
-                <Heading size="md">Yim</Heading>
-              </CardHeader>
-              <CardBody>
-                <Text>
-                  Apichat Aimimpak
-                  <br />
-                  ID | 65070501059
-                </Text>
-              </CardBody>
-              <CardFooter></CardFooter>
-            </Card>
-            <Card>
-              <CardHeader>
-                <img src="Wave.png" alt="Wave"></img>
-                <Heading size="md">Wave</Heading>
-              </CardHeader>
-              <CardBody>
-                <Text>
-                  Kanit Buntingkool
-                  <br />
-                  ID | 65070501064
-                </Text>
-              </CardBody>
-              <CardFooter></CardFooter>
-            </Card>
-          </Flex>
-        </Stack>
+          <div className="font-[Poppins]">Contact</div>
+        </Heading>
+        <div className="flex lg:flex-row flex-col gap-12 px-16">
+          {/* <Flex gap="10" flexDirection={{ lg: "row", base: "column" }}> */}
+
+          <Card boxShadow="xl">
+            <CardHeader>
+              <img src="Puen.png" alt="Puen"></img>
+              <Heading size="md">Puen</Heading>
+            </CardHeader>
+            <CardBody>
+              <Text>
+                Kittipong Tapyou
+                <br />
+                ID | 65070501003
+              </Text>
+            </CardBody>
+            <CardFooter></CardFooter>
+          </Card>
+          <Card boxShadow="xl">
+            <CardHeader>
+              <img src="Mo.png" alt="Mo"></img>
+              <Heading size="md">Mo</Heading>
+            </CardHeader>
+            <CardBody>
+              <Text>
+                Natlada Simasathien
+                <br />
+                ID | 65070501019
+              </Text>
+            </CardBody>
+            <CardFooter></CardFooter>
+          </Card>
+          <Card boxShadow="xl">
+            <CardHeader>
+              <img src="Tor.png" alt="Tor"></img>
+              <Heading size="md">Tor</Heading>
+            </CardHeader>
+            <CardBody>
+              <Text>
+                Wongsatorn Sungsilpawech
+                <br />
+                ID | 65070501050
+              </Text>
+            </CardBody>
+            <CardFooter></CardFooter>
+          </Card>
+          <Card boxShadow="xl">
+            <CardHeader>
+              <img src="Yim.png" alt="Yim"></img>
+              <Heading size="md">Yim</Heading>
+            </CardHeader>
+            <CardBody>
+              <Text>
+                Apichat Aimimpak
+                <br />
+                ID | 65070501059
+              </Text>
+            </CardBody>
+            <CardFooter></CardFooter>
+          </Card>
+          <Card boxShadow="xl">
+            <CardHeader>
+              <img src="Wave.png" alt="Wave"></img>
+              <Heading size="md">Wave</Heading>
+            </CardHeader>
+            <CardBody>
+              <Text>
+                Kanit Buntingkool
+                <br />
+                ID | 65070501064
+              </Text>
+            </CardBody>
+            <CardFooter></CardFooter>
+          </Card>
+        </div>
+      </Stack>
     </Flex>
   );
 };
