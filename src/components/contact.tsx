f2842c5ba31a988afcaf03f2f509ee7096ff6e5f
encall <apichataimimpat@gmail.com>
2022-12-19 02:56:47 +0700

    add contact page

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -9,8 +9,9 @@ import {
   CardBody,
   CardHeader,
   CardFooter,
-  SimpleGrid,
+  Box,
 } from "@chakra-ui/react";
+import { AiOutlineInstagram } from "react-icons/ai";
 
 const Contact = () => {
   return (
@@ -20,7 +21,6 @@ const Contact = () => {
       textAlign="center"
       minH="100vh"
     >
-      <Container maxW={"5xl"}>
         <Stack
           textAlign={"center"}
           align={"center"}
@@ -36,44 +36,87 @@ const Contact = () => {
           >
             Contact
           </Heading>
-          <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
-  <Card>
-    <CardHeader>
-        <img src="" alt=""></img>
-      <Heading size='md'> Customer dashboard</Heading>
-    </CardHeader>
-    <CardBody>
-      <Text>View a summary of all your customers over the last month.</Text>
-    </CardBody>
-    <CardFooter>
-      <Button>View here</Button>
-    </CardFooter>
-  </Card>
-  <Card>
-    <CardHeader>
-      <Heading size='md'> Customer dashboard</Heading>
-    </CardHeader>
-    <CardBody>
-      <Text>View a summary of all your customers over the last month.</Text>
-    </CardBody>
-    <CardFooter>
-      <Button>View here</Button>
-    </CardFooter>
-  </Card>
-  <Card>
-    <CardHeader>
-      <Heading size='md'> Customer dashboard</Heading>
-    </CardHeader>
-    <CardBody>
-      <Text>View a summary of all your customers over the last month.</Text>
-    </CardBody>
-    <CardFooter>
-      <Button>View here</Button>
-    </CardFooter>
-  </Card>
-</SimpleGrid>
+          <Flex
+            overflowY="scroll"
+            gap="10"
+            flexDirection={{ lg: "row", base: "column" }}
+          >
+            <Card boxShadow="xl">
+              <CardHeader>
+                <img src="Puen.png" alt="Puen"></img>
+                <Heading size="md">Puen</Heading>
+              </CardHeader>
+              <CardBody>
+                <Text>
+                  Kittipong Tapyou
+                  <br />
+                  ID | 65070501003
+                </Text>
+              </CardBody>
+              <CardFooter></CardFooter>
+            </Card>
+            <Card>
+              <CardHeader>
+                <img src="Mo.png" alt="Mo"></img>
+                <Heading size="md">Mo</Heading>
+              </CardHeader>
+              <CardBody>
+                <Text>
+                  Natlada Simasathien
+                  <br />
+                  ID | 65070501019
+                </Text>
+              </CardBody>
+              <CardFooter>
+                <Box boxShadow="base" alignContent="center" alignSelf="center" justifyContent="center">
+                  <Button as="a" href="https://www.instagram.com/encall_apichat/" leftIcon={<AiOutlineInstagram />} >Instagram</Button>
+                </Box>
+              </CardFooter>
+            </Card>
+            <Card>
+              <CardHeader>
+                <img src="Tor.png" alt="Tor"></img>
+                <Heading size="md">Tor</Heading>
+              </CardHeader>
+              <CardBody>
+                <Text>
+                  Wongsatorn Sungsilpawech
+                  <br />
+                  ID | 65070501050
+                </Text>
+              </CardBody>
+              <CardFooter></CardFooter>
+            </Card>
+            <Card>
+              <CardHeader>
+                <img src="Yim.png" alt="Yim"></img>
+                <Heading size="md">Yim</Heading>
+              </CardHeader>
+              <CardBody>
+                <Text>
+                  Apichat Aimimpak
+                  <br />
+                  ID | 65070501059
+                </Text>
+              </CardBody>
+              <CardFooter></CardFooter>
+            </Card>
+            <Card>
+              <CardHeader>
+                <img src="Wave.png" alt="Wave"></img>
+                <Heading size="md">Wave</Heading>
+              </CardHeader>
+              <CardBody>
+                <Text>
+                  Kanit Buntingkool
+                  <br />
+                  ID | 65070501064
+                </Text>
+              </CardBody>
+              <CardFooter></CardFooter>
+            </Card>
+          </Flex>
         </Stack>
-      </Container>
     </Flex>
   );
 };
